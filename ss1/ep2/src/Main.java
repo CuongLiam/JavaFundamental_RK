393f4ad1595da1b68021234cb2281e945c0782b4
Cuong Liam <cuongdeptrai2706@gmail.com>
2026-02-24 09:57:38 +0700

    ss1ep2

--- ss1/ep2/src/Main.java
+++ ss1/ep2/src/Main.java
@@ -1,15 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        int lateDays, borrowedBooks; // n, m
+        double lateFees, lateFees2 = 0;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Số ngày trễ: ");
+        lateDays = sc.nextInt();
+        System.out.println("Số lượng sách mượn: ");
+        borrowedBooks = sc.nextInt();
+
+        lateFees = (lateDays * borrowedBooks * 5000);
+        if (lateDays > 7 && borrowedBooks >=3){
+            lateFees2 += lateFees + lateFees * 0.2;
+        } else{
+            lateFees2 += lateFees;
         }
+
+        displayResult(lateFees, lateFees2);
+    }
+
+    static void displayResult(double lateFees, double lateFees2){
+        System.out.println("Tiền phạt gốc: "+lateFees);
+        System.out.println("tiền phạt sau điều chỉnh: "+lateFees2);
+        System.out.println("Yêu cầu khoá thẻ: "+((lateFees2 > 50000) ? "true" : "false"));
     }
 }
