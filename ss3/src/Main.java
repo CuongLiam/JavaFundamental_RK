17058aa3feabef7c2d8082c2bf239a2ba1103374
Cuong Liam <cuongdeptrai2706@gmail.com>
2026-03-05 07:32:52 +0700

    ss2,1 ep3

--- ss3/src/Main.java
+++ ss3/src/Main.java
@@ -1,12 +1,14 @@
 import ss3_redo.ep1;
-
+import ss3_redo.ep2;
 
 
 public class Main {
     public static void main(String[] args) {
 //        ss3ep1.ep1();
-        ep1.ep1Main();
 
+        // redo:
+//        ep1.ep1Main();
+        ep2.ep2Main();
 
     }
 }
--- ss3/src/ss3_redo/ep2.java
+++ ss3/src/ss3_redo/ep2.java
@@ -1,7 +1,33 @@
 package ss3_redo;
 
+import java.util.Scanner;
+
 public class ep2 {
     public static void ep2Main(){
-        
+        String[] books = new String[5];
+        books = new String[]{"Doraemon", "ABC", "123", "453", "sda"};
+
+//        for (String book : books) {
+//            System.out.println(book);
+//        }
+
+        System.out.println("Nhập tên sách cần tìm: ");
+        Scanner sc = new Scanner(System.in);
+        String search = sc.nextLine();
+
+        int res = searchBooks(books, search);
+
+        if (res != -1){
+            System.out.println("Found sach '"+search+"' tai vi tri so: "+res);
+        } else {
+            System.out.println("Sach k ton tai");
+        }
+
+    }
+    static int searchBooks(String[] arr, String search){
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i].equalsIgnoreCase(search)) return i;
+        }
+        return -1;
     }
 }
