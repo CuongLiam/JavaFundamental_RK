5476b564384b18f2eea364551cc8ea431bb4d06c
Cuong Liam <cuongdeptrai2706@gmail.com>
2026-03-17 03:11:06 +0700

    ss4 ep1 remastered

--- ss4/src/Main.java
+++ ss4/src/Main.java
@@ -1,8 +1,12 @@
+import ss4_redo.*;
+
 public class Main {
     public static void main(String[] args) {
 //        ss4ep1.ep1();
 
 
-        ss4ep4.ep4();
+//        ss4ep4.ep4();
+
+        ep1.ep1Main();
     }
 }
--- ss4/src/ss4_redo/ep1.java
+++ ss4/src/ss4_redo/ep1.java
@@ -1,10 +1,43 @@
 package ss4_redo;
 
 public class ep1 {
-    public static void ep1Main(){
-        String title = "     JAVA fundamental       hello ";
-        String author = "Henry something";
 
-        
+    static String capitalizeWords(String str){
+        String[] words = str.split(" ");
+
+        StringBuilder res = new StringBuilder();
+
+        for (String word : words){
+            if (word.length() > 0){
+                res.append(Character.toUpperCase(word.charAt(0)))
+                        .append(word.substring(1).toLowerCase())
+                        .append(" ");
+            }
+        }
+
+       return res.toString().trim();
+
+    }
+
+    public static void ep1Main(){
+        String title = "     JAVA   fundamental       hello ";
+        String author = "Henry something halo";
+        String str3 = new String("     JAVA   fundamental       hello ");
+
+//        StringBuilder str1 = new StringBuilder("Hello");
+//
+//        System.out.println(str1.append(" World"));
+
+
+
+
+        String str1 = title.trim().replaceAll("\\s+", " ");
+        String str2 = capitalizeWords(author);
+
+
+        String result = str1.concat(" - Tác giả: ").concat(str2);
+
+
+        System.out.println(result);
     }
 }
